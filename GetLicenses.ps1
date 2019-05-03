9d30058c8187b65128eb5783bb5c810be602531f
wmorian <wmorian@pm.me>
2019-05-03 11:40:55 +0200

    key is now an object of name and version

--- GetLicenses.ps1
+++ GetLicenses.ps1
@@ -1,5 +1,7 @@
 param(
-    [string]$Solution = ".", [string]$Output = ""
+    [Parameter(Mandatory = $true, Position = 0)]
+    [string]$Solution,
+    [string]$Output
 )
 
 if (!(Test-Path $Solution)) {
@@ -49,6 +51,7 @@ function GetLicenses {
 
     return $hash
 }
+
 function GetSubdependencies {
     param (
         $packageName, $packageVersion
@@ -58,7 +61,7 @@ function GetSubdependencies {
     $nuspecFile = "$env:USERPROFILE\.nuget\packages\$packageName\$packageVersion\$packageName.nuspec"        
 
     if (!(Test-Path $nuspecFile)) {
-        Write-Host "File not found: $nuspecFile"
+        Write-Host "File not found and will be ignored: $nuspecFile"
         $uniquePackages.Remove($packageName)
         continue
     }
@@ -71,8 +74,8 @@ function GetSubdependencies {
             $dependency.Version = $_.version.Trim('[()]')
         
             # Doesn't distinguish between same packages with different version numbers, the key should be a KeyValue with name and version
-            if (!$uniquePackages.Contains($dependency.Name)) {
-                $uniquePackages.Add($dependency.Name, $dependency.Version)
+            if (!$uniquePackages.Contains($dependency.GetHashCode())) {
+                $uniquePackages.Add($dependency.GetHashCode(), $dependency)
                 GetSubdependencies $dependency.Name $dependency.Version
             } 
         }
@@ -90,22 +93,34 @@ Invoke-Expression "$expr $solutionPath $command" |
 Where-Object { $_ -match ">" } |
 ForEach-Object {
     $packageInfo = $_.Split(' ', [System.StringSplitOptions]::RemoveEmptyEntries)
-    $Name = $packageInfo[1]
-    $Version = $packageInfo[3].Trim('[()]')
+    # $Name = $packageInfo[1]
+    # $Version = $packageInfo[3].Trim('[()]')
 
-    if (!$uniquePackages.Contains($Name)) {
-        $uniquePackages.Add($Name, $Version)
+    $dependency = "" | Select-Object "Name", "Version"
+    $dependency.Name = $packageInfo[1]
+    $dependency.Version = $packageInfo[3].Trim('[()]')
+
+    if (!$uniquePackages.Contains($dependency.GetHashCode())) {
+        $uniquePackages.Add($dependency.GetHashCode(), $dependency)
     }
 }
+
+# For Logging later
+$count = $uniquePackages.Count
 
 # copy the keys in an array, otherwise exception will be thrown during the iteration, because hashtable changes in the loop
 $keys = @($uniquePackages.Keys)
 
 foreach ($key in $keys) {
-    GetSubdependencies $key $uniquePackages[$key]
+    $dependency = $uniquePackages[$key]
+    GetSubdependencies $dependency.Name $dependency.Version
 }
 
-$packages = $uniquePackages.GetEnumerator() | Sort-Object -Property Name
+# Logging
+Write-Host "Direct dependencies found: $count"
+Write-Host "Subdependencies found: $($uniquePackages.Count - $count)"
+
+$packages = $uniquePackages.Values | Sort-Object -Property Name
 
 # get licenseUrls for all packages (and subpackages) in solution
 $licenses = (GetLicenses $packages)
