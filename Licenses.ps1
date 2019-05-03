1ee01a88a1e5f74b9ba21802daab59f0046e3357
wmorian <wmorian@pm.me>
2019-05-03 07:07:47 +0200

    retrieving subdependencies recursivley takes too long

--- Licenses.ps1
+++ Licenses.ps1
@@ -1,3 +1,24 @@
+$global:uniquePackages = [ordered]@{}
+
+function MergeHashTables {
+    param (
+        $table1, $table2
+    )
+
+    #!!!!!!!!!!! Changing the order of tables changes the result !!!!!!!!!!!!!!!
+    # Remove duplicate keys in hashtables, otherwise a merge is not possible
+    foreach ($key in $table1.Keys) {
+        if ($table2.Contains($key)) {
+            $table2.Remove($key)
+        }
+    }
+
+    # merging all entries in on table
+    $table2 = $table2 + $table1
+
+    return $table2
+}
+
 function GetLicenses {
     param (
         $packages
@@ -6,9 +27,6 @@ function GetLicenses {
     $hash = [ordered]@{ }
 
     foreach ($package in $packages.GetEnumerator()) {
-        # $packageName = $package.Name.ToLower()
-        # $packageVersion = $package.Version
-
         $packageName = $package.key.ToLower()
         $packageVersion = $package.value
 
@@ -39,18 +57,15 @@ function GetLicenses {
 
 function GetSubdependencies {
     param (
-        $package
+        $packageName, $packageVersion
     )
     
     $dependencies = [ordered]@{ }
-
-    $packageName = $package.Key.ToLower()
-    $packageVersion = $package.Value
-
+    $packageName = $packageName.ToLower()
     $nuspecFile = "$env:USERPROFILE\.nuget\packages\$packageName\$packageVersion\$packageName.nuspec"        
 
     if (!(Test-Path $nuspecFile)) {
-        Write-Host "File not found: $nuspecFile"
+        Write-Host "GETSUB: File not found: $nuspecFile"
         continue
     }
 
@@ -61,38 +76,28 @@ function GetSubdependencies {
             $dependency.Name = $_.id
             $dependency.Version = $_.version.Trim('[()]')
         
-            try {
+            if (!$dependencies.Contains($dependency.Name)) {
                 $dependencies.Add($dependency.Name, $dependency.Version)
             }
-            catch {
-                # Write-Host "GetSubdependencies: ignored duplicate package: $($dependency.Name, $dependency.Version)"
-            }    
+
+            # try {
+            #     $dependencies.Add($dependency.Name, $dependency.Version)
+            # }
+            # catch {
+            #     Write-Host "GetSubdependencies: ignored duplicate package: $($dependency.Name, $dependency.Version)"
+            # }    
         }
     }
 
-    # Get dependecies of Subdependencies
-    $subDependencies = [ordered]@{ }
-    if ($dependencies) {
-        foreach ($dep in $dependencies.GetEnumerator()) {
-            # Write-Host "$($pack.Name): $($pack.Value)"
-            $subDep = GetSubdependencies $dep
-            try {
-                $subDependencies = $subDependencies + $subDep
-            }
-            catch {
-                # Write-Host "ignored duplicate package: $($Name, $Version)"
-            }
-        }
-    }
+    if ($dependencies.Count -gt 0) {
+        $keys = $dependencies.Keys
 
-    # Remove duplicate packages in hashtables, otherwise a merge is not possible
-    foreach ($dep in $dependencies.Keys) {
-        if ($subDependencies.Contains($dep)) {
-            $subDependencies.Remove($dep)
+        foreach ($key in $keys) {
+            $subDep = GetSubdependencies $key $dependencies[$key]
+
+            $dependencies = MergeHashTables $dependencies $subDep
         }
     }
-    # merging all packages in on table
-    $dependencies = $dependencies + $subDependencies
 
     return $dependencies
 }
@@ -109,11 +114,12 @@ ForEach-Object {
     $packageInfo = $_.Split(' ', [System.StringSplitOptions]::RemoveEmptyEntries)
     $Name = $packageInfo[1]
     $Version = $packageInfo[3].Trim('[()]')
+
     try {
         $packages.Add($Name, $Version)    
     }
     catch {
-        # Write-Host "ignored duplicate package: $($Name, $Version)"
+        Write-Host "ignored duplicate package: $($Name, $Version)"
     }
     
 }
@@ -121,25 +127,12 @@ ForEach-Object {
 $dependencies = [ordered]@{ }
 
 foreach ($pack in $packages.GetEnumerator()) {
-    # Write-Host "$($pack.Name): $($pack.Value)"
-    $subDep = GetSubdependencies $pack
-    try {
-        $dependencies = $dependencies + $subDep
-    }
-    catch {
-        # Write-Host "ignored duplicate package: $($Name, $Version)"
-    }
+    $subDep = GetSubdependencies $pack.Name $pack.Value
+
+    $dependencies = MergeHashTables $dependencies $subDep
 }
 
-# Remove duplicate packages in hashtables, otherwise a merge is not possible
-foreach ($dep in $dependencies.Keys) {
-    if ($packages.Contains($dep)) {
-        $packages.Remove($dep)
-    }
-}
-# merging all packages in on table
-$packages = $packages + $dependencies
-
+$packages = MergeHashTables $dependencies $packages 
 $packages = $packages.GetEnumerator() | Sort-Object -Property Name
 
 # get licenseUrls for all packages (and subpackages) in solution
